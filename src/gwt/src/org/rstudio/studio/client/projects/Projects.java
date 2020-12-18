5edf5e8facd090359dfa40eaf858b7d40ead3fa4
Ricardo Wurmus <rekado@elephly.net>
2020-12-19 00:57:26 +0100

    handleConnection: Switch R version when switching projects.
    * src/cpp/session/SessionHttpMethods.cpp (handleConnection): Switch to
    the recorded R version when switching to a project; when closing a
    project switch back to the default.

--- src/gwt/src/org/rstudio/studio/client/projects/Projects.java
+++ src/gwt/src/org/rstudio/studio/client/projects/Projects.java
@@ -1136,7 +1136,8 @@ public class Projects implements OpenProjectFileEvent.Handler,
                         {
                            // perform quit
                            applicationQuit_.performQuit(null, saveChanges,
-                                 input.getProjectFile().getPath());
+                                                        input.getProjectFile().getPath(),
+                                                        input.getRVersion());
                         }
                      };
 
