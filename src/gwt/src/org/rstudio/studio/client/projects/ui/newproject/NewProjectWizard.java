007b29e0af1c6132d80f9ee81799d1c3519ece2d
Ricardo Wurmus <rekado@elephly.net>
2020-12-18 10:35:39 +0100

    NewProjectWizard: Unhide version selector widget.
    *
    src/gwt/src/org/rstudio/studio/client/projects/ui/newproject/NewProjectWizard.java:
    Do not hide the widget.

--- src/gwt/src/org/rstudio/studio/client/projects/ui/newproject/NewProjectWizard.java
+++ src/gwt/src/org/rstudio/studio/client/projects/ui/newproject/NewProjectWizard.java
@@ -71,7 +71,6 @@ public class NewProjectWizard extends Wizard<NewProjectInput,NewProjectResult>
            rVersions.getRVersionSpec());
          addLeftWidget(rVersionSelector_);
          rVersionSelector_.getElement().getStyle().setMarginRight(8, Unit.PX);
-         rVersionSelector_.setVisible(false);
       }
       
       openInNewWindow_ = new CheckBox(constants_.openNewSessionLabel());
