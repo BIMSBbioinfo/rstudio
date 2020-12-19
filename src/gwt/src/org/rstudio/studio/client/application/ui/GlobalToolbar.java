0bf91a24f1d087bc78b501e0493eea7f02a9b9d3
Ricardo Wurmus <rekado@elephly.net>
2020-12-19 23:33:16 +0100

    Add version switcher widget to toolbar.

--- src/gwt/src/org/rstudio/studio/client/application/ui/GlobalToolbar.java
+++ src/gwt/src/org/rstudio/studio/client/application/ui/GlobalToolbar.java
@@ -236,6 +236,10 @@ public class GlobalToolbar extends Toolbar
          ProjectPopupMenu projectMenu = new ProjectPopupMenu(
                sessionInfo, commands_, ElementIds.PROJECT_MENUBUTTON_TOOLBAR_SUFFIX);
          addRightWidget(projectMenu.getToolbarButton());
+
+         RVersionPopupMenu rVersionMenu = new RVersionPopupMenu(
+               sessionInfo, commands_);
+         addRightWidget(rVersionMenu.getToolbarButton());
       }
    }
 
